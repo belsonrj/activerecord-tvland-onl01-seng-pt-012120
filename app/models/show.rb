0f1c469546a5d7322294ee616da4d907584943fe
Rick Belson <belsonrj@aol.com>
2020-04-07 03:43:20 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
   
+  def actors_list
+    actors = []
+    array_of_characters = self.actors.map {|actor| actor.name}
+    array_of_shows = self.shows.map {|shows| shows.name}
+    role = array_of_characters.concat array_of_shows
+    roles << role.join(" - ")
+    roles
+  end
 end
